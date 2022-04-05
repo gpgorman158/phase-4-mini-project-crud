21761cda7671b13af234da8c34d959ea1a385776
Gerald Gorman <gpgorman158@gmail.com>
2022-04-05 16:14:53 -0400

    commit tree ad018ec4233919dc22ebb0ec9cf070d6a4bfa6bf

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,33 @@
+class SpicesController < ApplicationController
+
+    def index
+        render json: Spice.all
+    end
+
+    # def show
+    #     spice = Spice.find_by(id:params[:id])
+    #     render json: spice
+    # end
+
+    def create
+        render json: Spice.create(spice_params), status: :created
+    end
+
+    def update
+        spice = Spice.find_by(id:params[:id])
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = Spice.find_by(id:params[:id])
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
